f976e2cd8580bb266f018a8fa66dff7e8e4f7ccd
Thulasi kumar <131241186+ThulasiK7@users.noreply.github.com>
2025-03-03 09:40:46 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,13 +1,16 @@
-class Person{
-private String name;
-//setter
-void setname(String newname){name=newname;}
-//getter
-String getname(){return name;}
-}
-public class Main{
+import java.util.*;
+class Main{
 public static void main(String args[]){
-Person p=new Person();
-p.setname("Alice");
-System.out.println(p.getname());
+String str="Hello";
+String s="Hello";String s1=new String("Hello");
+System.out.print(str.equals(s));System.out.println(s==s1);
+System.out.println(s1.equals(s));int count=0;
+for(int i=0;i<s.length();i++){if(s.charAt(i)=='a' ||s.charAt(i)=='e' ||s.charAt(i)=='i' ||s.charAt(i)=='o' ||s.charAt(i)=='u' ||s.charAt(i)=='A' ||s.charAt(i)=='E' ||s.charAt(i)=='I' ||s.charAt(i)=='O' ||s.charAt(i)=='U' ){count++;
+System.out.println(s.charAt(i)+" "+count);}}
+
+
+
+
+
+
 }}
